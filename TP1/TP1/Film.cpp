7640362e5b230fdba4d21877f98aecb97eb11967
Kevin Alexander Bejarano Hernandez <kevin-alexander.bejarano-hernandez@polymtl.ca>
2023-09-19 23:39:18 -0400

    Un peu de FIlm

--- TP1/TP1/Film.cpp
+++ TP1/TP1/Film.cpp
@@ -1,3 +1,97 @@
+//#include "Film.h"
+//#include <iostream>
+//#include <iomanip>
+//
+//// TODO: Implémenter les méthodes de la classe Film ici.
+//
+//// Constructeurs et destructeur
+//film::film() { 
+//    film("",0,"",categorie::action,0);
+//}
+//
+//Film::Film(string titre, int anneeDeSortie, string realisateur, Categorie categorie, int duree) {
+//    titre_ = titre;
+//    anneeDeSortie_ = anneeDeSortie;
+//    realisateur_ = realisateur;
+//    categorie_ = categorie;
+//    duree_ = duree;
+//}
+//
+//Film::~Film() { }
+//
+//// Getters
+//string Film::getTitre() const { return titre_; }
+//
+//string Film::getRealisateur() const { return realisateur_; }
+//
+//int Film::getAnneeDeSortie() const { return anneeDeSortie_; }
+//
+//Categorie Film::getCategorie() const { return categorie_; }
+//
+//int Film::getDuree() const { return duree_; }
+////
+//vector<Acteur*> Film::getActeurs() const { return{}; }
+//
+//vector<Critique*> Film::getCritiques() const { return{}; }
+//
+//int Film::getNbActeurs() const { return ; }
+//
+//int Film::getNbCritiques() const { return ; }
+//
+//// Setters
+//void Film::setTitre(string titre) { titre_ = titre; }
+//
+//void Film::setRealisateur(string realisateur) { realisateur_ = realisateur; }
+//
+//void Film::setAnneeDeSortie(int anneeDeSortie) { anneeDeSortie_ = anneeDeSortie; }
+//
+//void Film::setCategorie(Categorie categorie) { categorie_ = categorie; }
+//
+//void Film::setDuree(int duree) { duree_ = duree; }
+//
+//void Film::setActeurs(vector<Acteur*> acteurs) {}
+//    
+//void Film::setCritiques(vector<Critique*> critiquesVec) {}
+//
+//// Méthodes fonctionnelles
+//Acteur* Film::trouverActeur(const string nom) const {
+//    return nullptr;
+//}
+//
+//Critique* Film::trouverCritique(const string nom) const {
+//    return nullptr;
+//}
+//
+//bool Film::isActeurPresent(string nomActeur) const {
+//    return false;
+//}
+//
+//bool Film::isCritiquePresent(string nomCritique) const {
+//    return false;
+//}
+//
+//bool Film::ajouterActeur(string nom, int anneeNaissance, string biographie) {
+//    return false;
+//}
+//
+//bool Film::ajouterCritique(string nom, string commentaire, int note) {
+//    return false;
+//}
+//
+//bool Film::retirerActeur(const string nom) {
+//    return false;
+//}
+//
+//bool Film::retirerCritique(const string nom) {
+//    return false;
+//}
+//
+//float Film::obtenirNoteMoyenne() const {
+//    return 0;
+//}
+//
+//// Méthodes d'affichage
+//void Film::afficher() const { }
 #include "Film.h"
 #include <iostream>
 #include <iomanip>
@@ -5,9 +99,15 @@
 // TODO: Implémenter les méthodes de la classe Film ici.
 
 // Constructeurs et destructeur
-Film::Film() { }
+Film::Film() {}
 
-Film::Film(string titre, int anneeDeSortie, string realisateur, Categorie categorie, int duree) { }
+Film::Film(string titre, int anneeDeSortie, string realisateur, Categorie categorie, int duree) {
+    titre_ = titre;
+    anneeDeSortie_ = anneeDeSortie;
+    realisateur_ = realisateur;
+    categorie_ = categorie;
+    duree_ = duree;
+}
 
 Film::~Film() { }
 
@@ -22,43 +122,59 @@ Categorie Film::getCategorie() const { return Categorie::HORREUR; }
 
 int Film::getDuree() const { return 0; }
 
-vector<Acteur*> Film::getActeurs() const { return {}; }
+vector<Acteur*> Film::getActeurs() const { return {acteurs_}; }
 
-vector<Critique*> Film::getCritiques() const { return {}; }
+vector<Critique*> Film::getCritiques() const { return {critiques_}; }
 
-int Film::getNbActeurs() const { return 0; }
+int Film::getNbActeurs() const { return acteurs_.size(); }
 
-int Film::getNbCritiques() const { return 0; }
+int Film::getNbCritiques() const { return critiques_.size(); }
 
 // Setters
-void Film::setTitre(string titre) { }
+void Film::setTitre(string titre) { titre_ = titre; }
 
-void Film::setRealisateur(string realisateur) { }
+void Film::setRealisateur(string realisateur) { realisateur_ = realisateur; }
 
-void Film::setAnneeDeSortie(int anneeDeSortie) { }
+void Film::setAnneeDeSortie(int anneeDeSortie) { anneeDeSortie_ = anneeDeSortie; }
 
-void Film::setCategorie(Categorie categorie) { }
+void Film::setCategorie(Categorie categorie) { categorie_ = categorie; }
 
-void Film::setDuree(int duree) { }
+void Film::setDuree(int duree) { duree_ = duree; }
 
-void Film::setActeurs(vector<Acteur*> acteurs) { }
+void Film::setActeurs(vector<Acteur*> acteurs) {}
 
-void Film::setCritiques(vector<Critique*> critiquesVec) { }
+void Film::setCritiques(vector<Critique*> critiquesVec) {}
 
 // Méthodes fonctionnelles
 Acteur* Film::trouverActeur(const string nom) const {
+    for (auto& acteur : acteurs_) 
+        if (acteur->getNom() == nom)
+            return acteur;
+
     return nullptr;
 }
 
 Critique* Film::trouverCritique(const string nom) const {
+    for (auto& critique : critiques_) 
+        if (critique->getAuteur() == nom)
+            return critique;
+       
     return nullptr;
 }
 
 bool Film::isActeurPresent(string nomActeur) const {
+    
+    if (trouverActeur(nomActeur) != nullptr)
+            return true;
+        
     return false;
 }
 
 bool Film::isCritiquePresent(string nomCritique) const {
+    
+    if (trouverCritique(nomCritique) != nullptr)
+        return true;
+    
     return false;
 }
 
