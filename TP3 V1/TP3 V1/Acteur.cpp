19a0b2dd2b042db9b6be9848999f2437cc825c4b
Kevin Alexander Bejarano Hernandez <kevin-alexander.bejarano-hernandez@polymtl.ca>
2023-10-26 09:04:31 -0400

    Acteur

--- TP3 V1/TP3 V1/Acteur.cpp
+++ TP3 V1/TP3 V1/Acteur.cpp
@@ -1,7 +1,7 @@
 #include "Acteur.h"
 
 // Constructeurs
-Acteur::Acteur() :Cast() {} //TODO
+Acteur::Acteur() :Cast(), agence_("") {} //TODO
 
 Acteur::Acteur(const string& nom, int anneeNaissance, const string& biographie, int salaire, const string& agence): Cast(nom, anneeNaissance, biographie, salaire), agence_(agence) {}  //TODO
 
@@ -10,14 +10,29 @@ Acteur::Acteur(const Acteur& acteur) : Cast(acteur.nom_, acteur.anneeNaissance_,
 // Methodes virtuelles surchargees
 bool Acteur::accepterRole(const pair<int, Cast*>& role) const { 
 	
-	//if(role.first>= salaire_ && )
+	if (role.first >= salaire_ && dynamic_cast<const Acteur*>(role.second) != nullptr)
+		return true;
+
 	return false;
 }  //TODO
 
-ClasseCelebrite Acteur::calculerPopularite() const { return ClasseCelebrite::B_CELEBRITY; }  //TODO
+ClasseCelebrite Acteur::calculerPopularite() const { 
+	int roles = static_cast<int>(rolesJoues_.size());
+
+	if(roles > SEUIL_A)
+		return ClasseCelebrite::A_CELEBRITY;
+	else if (roles >= SEUIL_B && roles <= SEUIL_A )
+		return ClasseCelebrite::B_CELEBRITY;
+	else if (roles < SEUIL_B)
+		return ClasseCelebrite::C_CELEBRITY;
+	
+}  //TODO
 
 // Operator
-Acteur& Acteur::operator+=(const string& role) { return *this;}  //TODO
+Acteur& Acteur::operator+=(const string& role) { 
+	this->ajouterRole(role);
+	return *this;
+}  //TODO
 
 // Methode d'affichage
 void Acteur::afficherRoles() const { } //TODO
